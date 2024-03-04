cbdc69aa2a62a958d87f499a76680a03ffe625f0
nino <nino.courtois@epfedu.fr>
2024-03-04 12:43:26 +0100

    Ajout de l'image de l'ascenceur, problème à l'afficher car trop grand 12:44 04/03

--- src/js/selection.js
+++ src/js/selection.js
@@ -25,6 +25,7 @@ export default class selection extends Phaser.Scene {
   preload() {
     // tous les assets du jeu sont placés dans le sous-répertoire src/assets/
     this.load.image("img_ciel", "src/assets/sky.png");
+    this.load.image("img_lift", "src/assets/lift.png");
     this.load.image("img_fond_1", "src/assets/doorsbackground.png");
     this.load.image("img_plateforme", "src/assets/platform.png");
     this.load.spritesheet("img_perso", "src/assets/dude.png", {
