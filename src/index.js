8d3f27167b107eeff5c35284d32b6fbe14705287
nino <nino.courtois@epfedu.fr>
2024-03-04 15:15:26 +0100

    que la tete du perso 15:15 04/03

--- src/index.js
+++ src/index.js
@@ -20,7 +20,7 @@ var config = {
     arcade: {
       // parametres du mode arcade
       gravity: {
-        y: 1 // gravité verticale : acceleration ddes corps en pixels par seconde
+        y: 0 // gravité verticale : acceleration ddes corps en pixels par seconde
       },
       debug: false // permet de voir les hitbox et les vecteurs d'acceleration quand mis à true
     }
--- src/js/selection.js
+++ src/js/selection.js
@@ -24,13 +24,14 @@ export default class selection extends Phaser.Scene {
    */
   preload() {
     // tous les assets du jeu sont placés dans le sous-répertoire src/assets/
+    this.load.image("img_souris", "src/assets/souris.png");
     this.load.image("img_ciel", "src/assets/sky.png");
     this.load.image("img_lift", "src/assets/lift.png");
     this.load.image("img_fond_1", "src/assets/doorsbackground.png");
     this.load.image("img_plateforme", "src/assets/platform.png");
     this.load.spritesheet("img_perso", "src/assets/dude.png", {
       frameWidth: 32,
-      frameHeight: 48
+      frameHeigh: 48
     });
     this.load.image("img_porte1", "src/assets/door1.png");
     this.load.image("img_porte2", "src/assets/door2.png");
