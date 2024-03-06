5eeca9a8f0f0152e5a699b5aee4f2567ee350e20
nino <nino.courtois@epfedu.fr>
2024-03-06 12:07:18 +0100

    Niveau 2 qui marche!

--- src/js/niveau2.js
+++ src/js/niveau2.js
@@ -62,9 +62,9 @@ this.load.spritesheet("dead", "src/assets/jaune_meurt.png", {
 
     this.physics.add.collider(this.player2, calque_background_2);
 
-    this.physics.world.setBounds(0, 0, 3200, 640);
+    this.physics.world.setBounds(0, 0, 3200, 1200);
     //  ajout du champs de la caméra de taille identique à celle du monde
-    this.cameras.main.setBounds(0, 0, 3200, 640);
+    this.cameras.main.setBounds(0, 0, 3200, 1200);
     // ancrage de la caméra sur le joueur
     this.cameras.main.startFollow(this.player2);
     // player.setCollideWorldBounds(true);
